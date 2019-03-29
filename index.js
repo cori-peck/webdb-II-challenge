cd265076865021387726e8bc443d29f5798087c8
cori-peck <cori.peck@ymail.com>
2019-03-29 15:11:50 -0500

    Knex is configured to connect to data file. R-Read the list of zoos is functional

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +12,15 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos)
+  } catch (err) {
+    res.status(500).json({ message: "Could not retrieve zoos" })
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
