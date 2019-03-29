14e6a807ea0d310dfad0e74510459f9562d9e1ba
cori-peck <cori.peck@ymail.com>
2019-03-29 16:45:11 -0500

    Get by id is functional and add(post) a zoo works

--- index.js
+++ index.js
@@ -13,12 +13,41 @@ server.use(helmet());
 
 // endpoints here
 
+// C
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const [ id ] = await db('zoos')
+      .insert(req.body);
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+    res.status(201).json(zoo)
+  } catch (err) {
+    res.status(500).json({ message: 'Could not add zoo' })
+  }
+})
+
+// R
 server.get('/api/zoos', async (req, res) => {
   try {
     const zoos = await db('zoos');
     res.status(200).json(zoos)
   } catch (err) {
     res.status(500).json({ message: "Could not retrieve zoos" })
+  }
+})
+
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos')
+    .where({ id: req.params.id})
+    if (zoo.length === 0) {
+      res.status(404).json({ error: "That id does not exist" })
+    } else {
+      res.status(200).json(zoo);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Could not retrieve requested zoo" })
   }
 })
 
