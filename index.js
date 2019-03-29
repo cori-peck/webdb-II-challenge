b6de9ba6481a49788de7441e8fee9cfaa22805ca
cori-peck <cori.peck@ymail.com>
2019-03-29 17:15:42 -0500

    Update (put) is functional

--- index.js
+++ index.js
@@ -51,6 +51,25 @@ server.get('/api/zoos/:id', async (req, res) => {
   }
 })
 
+// U
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const rec = await db('zoos')
+    .where({ id: req.params.id })
+    .update(req.body);
+    if (rec === 0) {
+      res.status(404).json({ error: "That id does not exist" })
+    } else {
+      const zoo = await db('zoos')
+      .where({ id: req.params.id })
+      .first();
+      res.status(200).json(zoo);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
